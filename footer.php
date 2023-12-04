28031bd65d7f3b00717b97e030403cd9118bb7df
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-12-04 14:11:34 +0100

    Starting files

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- <script src="src/js/script.js"></script> -->
+
+<footer>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Title of the document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- local css -->
+    <link rel="stylesheet" href="src/style/style.css">
+</head>
+
+<body>
+
+</body>
