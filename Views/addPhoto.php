4f70cd066540892f8b0d74eff51641cec160a10c
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-12-22 13:20:30 +0100

    Add photo page
    Added add photo page with small form (for further changes)
    Added link to add photo page
    Changed main element css

---
+++ Views/addPhoto.php
@@ -0,0 +1,13 @@
+<?php require '../nav.php' ?>
+
+<main class="addPhoto">
+    <h2>Dodaj zdjęcie</h2>
+    <section>
+        <form action="" class="fileUpload">
+            <input type="file" name="file" id="file" accept=".jpeg, .jpg, .png, image/jpeg, image/png">
+            <label for="file"><i class="fa-solid fa-file-image"></i> Wybierz zdjęcie</label>
+        </form>
+    </section>
+</main>
+
+<?php require '../footer.php' ?>
--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
             <li><a href="index.php"><i class="fa-solid fa-house"></i><span>Strona główna</span></a></li>
             <li><a href="search.php"><i class="fa-solid fa-magnifying-glass"></i><span>Szukaj</span></a></li>
             <li><a href="favourites.php"><i class="fa-regular fa-heart"></i><span>Polubione</span></a></li>
-            <li><a href="#.php"><i class="fa-solid fa-plus"></i><span>Dodaj</span></a></li>
+            <li><a href="addPhoto.php"><i class="fa-solid fa-plus"></i><span>Dodaj</span></a></li>
             <li><a href="#.php"><i class="fa-solid fa-user"></i><span>Profil</span></a></li>
         </ul>
     </nav>
