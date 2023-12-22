70dd739f6198b36d2809a40225776165aab5e59c
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-12-22 13:00:23 +0100

    Favourites page
    Added responsive favourites page
    Added link for favourites in menu

---
+++ Views/favourites.php
@@ -0,0 +1,18 @@
+<?php require '../nav.php' ?>
+
+<main class="favourites">
+    <h2>Twoje ulubione</h2>
+    <section>
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+        <article class="post">
+            <a href="#.php">
+                <i class="fa-solid fa-user"></i>
+                <span>User Login</span>
+            </a>
+            <div class="img"></div>
+        </article>
+        <?php endfor; ?>
+    </section>
+</main>
+
+<?php require '../footer.php' ?>
