6d07d18cde0acf0057281edc412ca3a257183f5e
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-12-26 11:52:03 +0100

    Added dbh_classes.php
    Added file for connection with database

---
+++ Classes/dbh_classes.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+class Dbh
+{
+    const USERNAME = 'root';
+    const PASSWORD = '';
+
+    const DBNAME = 'photo_album';
+
+    protected function connect(){
+        try {
+            $dbh = new  PDO('mysql:host=localhost;dbname='.self::DBNAME, self::USERNAME, self::PASSWORD);
+            echo 'done';
+            return $dbh;
+        }
+        catch(PDOException $e) {
+            echo "Error!: ".$e->getMessage()."<br />";
+            die();
+        }
+    }
+}
