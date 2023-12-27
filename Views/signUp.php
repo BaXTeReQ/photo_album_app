29022f8a7c79f3fdef091d2cb03bdba5a6f96495
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2023-12-27 13:06:10 +0100

    PHP code update in sign up page

--- Views/signUp.php
+++ Views/signUp.php
@@ -11,46 +11,31 @@
             <label for="email">E-mail</label>
             <input name="email" id="email" type="email" required="" placeholder="email@example.com">
 
-            <?php 
-                if (isset($_GET['emailTakenError'])) {
-                    echo "<label class='red' for='email'>";
-                    echo "E-mail already taken"; 
-                    echo "</label>";
-                }
-            ?>
+            <?php if (isset($_GET['emailTakenError'])): ?>
+            <label class='red' for='email'>E-mail already taken</label>
+            <?php endif; ?>
 
             <label for="login">Login</label>
             <input name="login" id="login" type="text" required="" placeholder="User123#!">
 
-            <?php 
-                if (isset($_GET['usernameTakenError'])) {
-                echo "<label class='red' for='login'>";
-                echo "Login already taken"; 
-                echo "</label>";
-                }
-            ?>
+            <?php if (isset($_GET['usernameTakenError'])): ?>
+            <label class='red' for='login'>Login already taken</label>
+            <?php endif; ?>
 
             <label for="password">Password</label>
             <input name="password" id="password" type="password" required="">
 
-            <?php 
-                if (isset($_GET['invalidpasswordError'])) {
-                    echo "<label class='red' for='password'>";
-                    echo "Password must be minimum 8 characters long and have uppercase letters, lowercase letters, numbers and symbols"; 
-                    echo "</label>";
-                }
-            ?>
+            <?php if (isset($_GET['invalidpasswordError'])): ?>
+            <label class='red' for='password'>Password must be minimum 8 characters long and have uppercase letters,
+                lowercase letters, numbers and symbols</label>
+            <?php endif; ?>
 
             <label for="confirm_password">Confirm Password</label>
             <input name="confirm_password" id="confirm_password" type="password" required="">
 
-            <?php 
-                if (isset($_GET['password!matchError'])) {
-                    echo "<label class='red' for='confirm_password'>";
-                    echo "Passwords don`t match";                  
-                    echo "</label>";
-                }
-            ?>
+            <?php if (isset($_GET['password!matchError'])): ?>
+            <label class='red' for='confirm_password'>Passwords don`t match</label>
+            <?php endif; ?>
 
             <button type="submit" name="register_submit">Register</button>
         </form>
