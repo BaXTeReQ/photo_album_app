e48eda895e28b88b4ae06acdeab0f93bb475824e
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-01-05 11:15:49 +0100

    Photos for user recommendations
    Profile photos in user recommendations are now working with IPFS

--- Views/userRecommendations.php
+++ Views/userRecommendations.php
@@ -3,15 +3,18 @@
     <section>
         <ul>
             <?php
-            require_once '../Classes/user_classes.php';
-            if (isset($_SESSION['userid'])) $users = User::getRecommendedUsers($_SESSION['userid']);
-            else $users = User::getRecommendedUsers();
+            require_once('../Classes/user_classes.php');
+            require_once('../Classes/ipfs_classes.php');
 
+            $users = (isset($_SESSION['userid'])) ? User::getRecommendedUsers($_SESSION['userid']) : User::getRecommendedUsers();
             foreach ($users as $user) :
+                $CID = User::getProfilePictureCID($user->getUserID());
+                $ipfs = new IPFS();
+                $gateway = $ipfs->getGateway();
             ?>
                 <li>
                     <a href="user.php?userid=<?php echo $user->getUserID(); ?>">
-                        <img src="../pictures/default_user_profile.png" alt="User Image" class="profile__photo"><?php echo $user->getUsername(); ?>
+                        <img src="<?php echo $gateway . $CID; ?>" alt="User Image" class="profile__photo"><?php echo $user->getUsername(); ?>
                     </a>
                 </li>
             <?php endforeach; ?>
