12309a57ab89fa0aba9b382897748ec9fbdd0bb1
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-01-09 17:19:07 +0100

    Added css to liking photo icon

--- src/js/photoLike.js
+++ src/js/photoLike.js
@@ -2,17 +2,19 @@ $(document).ready(function () {
     $(".like--button").on("click", function () {
         var photo_id = $(this).data("photo-id");
         var user_id = $(this).data("user-id");
+        var icon = $(this).find('i');
 
         // Send AJAX request to your PHP script
         $.ajax({
             type: "POST",
-            url: "../Includes/like_includes.php", // Update with your PHP script path
+            url: "../Includes/like_includes.php",
             data: {
                 user_id: user_id,
                 photo_id: photo_id
             },
             success: function (response) {
-                // console.log("Wpisano"); // Log the server response (for debugging)
+                icon.toggleClass('fa-solid fa-heart');
+                icon.toggleClass('fa-regular fa-heart');
             }
         });
     });
