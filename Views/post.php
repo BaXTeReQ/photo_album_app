80ac2dad3d92ad63edda62f989e493623c6f337b
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-01-09 15:03:54 +0100

    Like button for post

--- Views/post.php
+++ Views/post.php
@@ -4,6 +4,9 @@
         <span>User Login</span>
     </a>
     <div class="img"></div>
+    <button type="button">
+        <i class="fa-regular fa-heart"></i>
+    </button>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugiat explicabo dolore natus inventore! Facere illum
         accusamus necessitatibus minima, ex itaque voluptates! Quo eaque facere iusto veritatis sit. Nobis, voluptas!
     </p>
