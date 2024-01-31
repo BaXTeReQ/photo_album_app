2a5f2ed04a027788a1972e3c983790126481272f
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-01-31 12:43:42 +0100

    Update database
    Deleted tables for profile photos and uploaded photos
    Changed table name from photos to posts
    Deleted photoName column and added column for user ID in table posts
    Added profile_photoCID column to user table

--- database/photo_album.sql
+++ database/photo_album.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sty 03, 2024 at 06:31 PM
+-- Generation Time: Sty 31, 2024 at 12:40 PM
 -- Wersja serwera: 10.4.28-MariaDB
 -- Wersja PHP: 8.2.4
 
@@ -30,20 +30,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `favourites` (
   `ID` int(11) NOT NULL,
   `fk_userID` int(11) NOT NULL,
-  `fk_photoID` int(11) NOT NULL
+  `fk_postCID` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `photos`
+-- Struktura tabeli dla tabeli `posts`
 --
 
-CREATE TABLE `photos` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE `posts` (
   `CID` text NOT NULL,
-  `photoName` varchar(30) NOT NULL,
-  `description` text NOT NULL
+  `description` text DEFAULT NULL,
+  `fk_userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -77,6 +76,7 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(30) NOT NULL,
+  `profile_photoCID` text NOT NULL,
   `fk_roleID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -84,41 +84,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`ID`, `username`, `email`, `password`, `fk_roleID`) VALUES
-(1, 'admin', 'admin@example.com', 'admin', 1),
-(2, 'moderator', 'moderator@example.com', 'moderator', 2),
-(3, 'user_test', 'user_test123@example.com', 'Qwerty!23', 3),
-(4, 'test123', 'test123@email.com', 'Qwerty!23', 3),
-(5, 'jan_kowal', 'jankowalski@example.com', 'Qwerty!23', 3),
-(6, 'stefcio1234', 'stefan1234@example.com', 'Stefcio!234', 3),
-(7, 'aadadadadad', 'adambakowicz.vp.pl@vp.pl', 'Qwerty!23', 3),
-(8, 'grazka87', 'grazynamoc@example.com', 'Qwerty!23', 3),
-(9, 'jasia_m', 'jasiam@example.com', 'Qwerty!23', 3),
-(10, 'janek12', 'janekkowal@example.com', 'Qwerty!23', 3);
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `users_profile_photos`
---
-
-CREATE TABLE `users_profile_photos` (
-  `ID` int(11) NOT NULL,
-  `fk_userID` int(11) NOT NULL,
-  `fk_photoID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `users_uploaded_photos`
---
-
-CREATE TABLE `users_uploaded_photos` (
-  `ID` int(11) NOT NULL,
-  `fk_userID` int(11) NOT NULL,
-  `fk_photoID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `users` (`ID`, `username`, `email`, `password`, `profile_photoCID`, `fk_roleID`) VALUES
+(1, 'admin', 'admin@example.com', 'admin', 'Qmb6WUqrY2GVDMMxoJk9Fe7qRHVrLWUMpYb9tsVeaDTGCC', 1),
+(2, 'moderator', 'moderator@example.com', 'moderator', 'Qmb6WUqrY2GVDMMxoJk9Fe7qRHVrLWUMpYb9tsVeaDTGCC', 2),
+(3, 'user_test', 'user_test123@example.com', 'Qwerty!23', 'Qme3QMbvhKV22TSYv2N8fZFPN6TXHjSKAhkKVDuBkLshrw', 3),
+(4, 'test123', 'test123@email.com', 'Qwerty!23', 'QmNSH4VUEgxY7at3rJsKS8BUQuLDrFh5coqaG3M3W3GJdo', 3),
+(5, 'jan_kowal', 'jankowalski@example.com', 'Qwerty!23', 'QmP4AmDYo9oqWDzE1WzNeow6HSenR1GTaqSwWQYYM51m6A', 3),
+(6, 'stefcio1234', 'stefan1234@example.com', 'Stefcio!234', 'QmSkrZhhGiFPUELdPHT3S4N5Fi3FBYz1w5cpgR771HnxXr', 3),
+(7, 'aadadadadad', 'adambakowicz.vp.pl@vp.pl', 'Qwerty!23', 'QmXZTaANxPP8NkG3efgCStm3aFnD63iUoFqogFyMFUSzDb', 3),
+(8, 'grazka87', 'grazynamoc@example.com', 'Qwerty!23', 'QmNVdGkuWeeEtf7918GCQGS1k5B8ZB56L2zeytfxuahvvZ', 3),
+(9, 'jasia_m', 'jasiam@example.com', 'Qwerty!23', 'QmZbCmzXsUDA6zuSTHqQNc3dQzxr6nEzVTLv7DH1H3sa34', 3),
+(10, 'janek12', 'janekkowal@example.com', 'Qwerty!23', 'QmSNBDrnC5q7DaFhZjnzqBwiViFKo8H8DakGbSq3bsEnJn', 3),
+(11, 'adamsek123', 'adamsek@example.com', 'Qwerty!23', 'QmeZ78pPC6QVY8F1xdEg5tTRBMtvX1oj1yCYBchniZuMfY', 3);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -128,15 +105,13 @@ CREATE TABLE `users_uploaded_photos` (
 -- Indeksy dla tabeli `favourites`
 --
 ALTER TABLE `favourites`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_userID` (`fk_userID`) USING BTREE,
-  ADD KEY `fk_photoID` (`fk_photoID`) USING BTREE;
+  ADD PRIMARY KEY (`ID`);
 
 --
--- Indeksy dla tabeli `photos`
+-- Indeksy dla tabeli `posts`
 --
-ALTER TABLE `photos`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`CID`(50)) USING BTREE;
 
 --
 -- Indeksy dla tabeli `roles`
@@ -152,61 +127,30 @@ ALTER TABLE `users`
   ADD KEY `users_roles` (`fk_roleID`);
 
 --
--- Indeksy dla tabeli `users_profile_photos`
---
-ALTER TABLE `users_profile_photos`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_photoID` (`fk_photoID`),
-  ADD KEY `fk_userID` (`fk_userID`);
-
---
--- Indeksy dla tabeli `users_uploaded_photos`
---
-ALTER TABLE `users_uploaded_photos`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_photoID` (`fk_photoID`),
-  ADD KEY `fk_userID` (`fk_userID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `favourites`
+--
+ALTER TABLE `favourites`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `favourites`
---
-ALTER TABLE `favourites`
-  ADD CONSTRAINT `photos_ID` FOREIGN KEY (`fk_photoID`) REFERENCES `photos` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_ID` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_roles` FOREIGN KEY (`fk_roleID`) REFERENCES `roles` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users_profile_photos`
---
-ALTER TABLE `users_profile_photos`
-  ADD CONSTRAINT `fk_photoID` FOREIGN KEY (`fk_photoID`) REFERENCES `photos` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_userID` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users_uploaded_photos`
---
-ALTER TABLE `users_uploaded_photos`
-  ADD CONSTRAINT `users_uploaded_photos_ibfk_1` FOREIGN KEY (`fk_photoID`) REFERENCES `photos` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_uploaded_photos_ibfk_2` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
