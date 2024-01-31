f29bb40135519f44633fc7ef75c2d411384b242d
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-01-31 15:15:43 +0100

    Deleted comment

--- Classes/signIn_classes.php
+++ Classes/signIn_classes.php
@@ -9,7 +9,7 @@ class SignIn extends Dbh
     private string $username;
     private string $password;
 
-    public function __construct(string $username, string $password) // :self
+    public function __construct(string $username, string $password)
     {
         $this->username = $username;
         $this->password = $password;
