d4444697f1c9ed7decf780803f340eaab706a08d
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-02-10 09:59:38 +0100

    Post changes
    Updated file with post
    Included post file instead of whole code on index page

--- Views/index.php
+++ Views/index.php
@@ -12,26 +12,13 @@
         $ipfs = new IPFS();
         $gateway = $ipfs->getGateway();
 
-        foreach ($posts as $post) :
+        foreach ($posts as $post) {
             $user = new User($post->getUserID());
             $profilePictureCID = $user->getProfilePhotoCIDById($post->getUserID());
+
+            include '../Views/post.php';
+        }
         ?>
-        <article class="post">
-            <a href="user.php?userid=<?php echo $post->getUserID(); ?>">
-                <img src="<?php echo $gateway . $profilePictureCID; ?>" alt="User Image" class="profile__photo">
-                <span><?php echo $user->getUsernameById($post->getUserID()); ?></span>
-            </a>
-            <div class="img">
-                <img src="<?php echo $gateway . $post->getCID(); ?>" alt="<?php echo $post->getDescription(); ?>">
-            </div>
-            <button type="button" class="like--button" data-post-cid="<?php echo $post->getCID(); ?>"
-                data-post-liked="<?php echo ($post->checkIfPostIsLiked($_SESSION['userid'], $post->getCID())) ? 1 : 0; ?>">
-                <i class='<?php echo ($post->checkIfPostIsLiked($_SESSION['userid'], $post->getCID())) ? "fa-solid fa-heart" : "fa-regular fa-heart";
-                                ?>'></i>
-            </button>
-            <p><?php echo $post->getDescription(); ?></p>
-        </article>
-        <?php endforeach; ?>
     </section>
 </main>
 
--- Views/post.php
+++ Views/post.php
@@ -1,28 +1,15 @@
-<?php
-require_once('../Classes/post_classes.php');
-require_once('../Classes/ipfs_classes.php');
-?>
-
-<?php
-$posts = Post::getPosts();
-$ipfs = new IPFS();
-$gateway = $ipfs->getGateway();
-
-foreach ($posts as $post) :
-?>
-
 <article class="post">
     <a href="user.php?userid=<?php echo $post->getUserID(); ?>">
-        <img src="<?php echo User::getProfilePictureCID($userID); ?>" alt="User Image" class="profile__photo">
-        <span><?php echo $post->getUsername(); ?></span>
+        <img src="<?php echo $gateway . $profilePictureCID; ?>" alt="User Image" class="profile__photo">
+        <span><?php echo $user->getUsernameById($post->getUserID()); ?></span>
     </a>
     <div class="img">
-        <img src="<?php echo $gateway . $CID; ?>" alt="<?php echo $post->getDescription(); ?>">
+        <img src="<?php echo $gateway . $post->getCID(); ?>" alt="<?php echo $post->getDescription(); ?>">
     </div>
-    <button type="button" class="like--button" data-user-id="<?php echo $post->getUserID(); ?>" data-photo-id="1">
-        <i class="fa-regular fa-heart"></i>
+    <button type="button" class="like--button" data-post-cid="<?php echo $post->getCID(); ?>"
+        data-post-liked="<?php echo ($post->checkIfPostIsLiked($_SESSION['userid'], $post->getCID())) ? 1 : 0; ?>">
+        <i class='<?php echo ($post->checkIfPostIsLiked($_SESSION['userid'], $post->getCID())) ? "fa-solid fa-heart" : "fa-regular fa-heart";
+                    ?>'></i>
     </button>
     <p><?php echo $post->getDescription(); ?></p>
 </article>
-
-<?php endforeach; ?>
