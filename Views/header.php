ec4565a7af742dd307c86af354f57bd9eafc5724
BaXTeReQ <adambakowicz.vp.pl@vp.pl>
2024-04-25 21:29:53 +0200

    Fix for icons

--- Views/header.php
+++ Views/header.php
@@ -6,7 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet"> <!-- fonts -->
-    <script src="https://kit.fontawesome.com/5320478ffc.js" crossorigin="anonymous"></script> <!-- icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <!-- local css -->
     <link rel="stylesheet" href="../src/style/_normalize.css">
